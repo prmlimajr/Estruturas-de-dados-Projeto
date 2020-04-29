82c9697ca12cef22a1580b78ab16d9913fac429a
Paulo Lima <47723610+prmlimajr@users.noreply.github.com>
2020-04-28 21:19:10 -0300

    Create No.java

---
+++ No.java
@@ -0,0 +1,15 @@
+public class No {
+	
+	public double info;
+	public No prox;
+	
+	public No (double v) {
+		info = v;
+		prox = null;
+	}
+	
+	public No(double v, No p) {
+		info = v;
+		prox = p;
+	}
+}
