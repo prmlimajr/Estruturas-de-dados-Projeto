3109cd9d5d89816f20bc5eab375d9ab0814ad560
Paulo Lima <47723610+prmlimajr@users.noreply.github.com>
2020-04-28 23:29:54 -0300

    Create Pilha_LinkedList.java

---
+++ Pilha_LinkedList.java
@@ -0,0 +1,77 @@
+public class Pilha_LinkedList {
+	
+	public No_pilha ini, fim;
+	public int livre, max;
+	
+	public Pilha_LinkedList(){
+		ini = null;
+		fim = null;
+		livre = 0;
+		max = 1000;
+	}
+	
+	public Pilha_LinkedList(int valor) {
+		ini = null;
+		fim = null;
+		livre = 0;
+		max = 1000;		
+	}
+	
+	public int size(){
+		return livre;
+	}
+	
+	public void push(char e){
+	    if(size() == 0){
+	        ini = new No_pilha(e);
+	        fim = ini;
+	        livre++;
+	    } else if((size() != 0) && (size() < max)) {
+	        No_pilha newNode = new No_pilha(e, ini);
+	        ini = newNode;
+	        livre++;	    	
+	    } else {
+	    	System.out.println("A pilha está cheia!");
+	    }
+	}
+	
+    public char pop(){
+    	if(size() != 0){
+    		char removed = ini.info;
+    		ini = ini.prox;
+    		livre--;
+    		return removed;
+    	} else {
+    		System.out.println("A pilha está vazia!");
+    		return '.';
+    	}
+    }
+	
+    public char top() {
+        if (size() != 0){
+            return ini.info;
+        } else{
+            System.out.println("A pilha está vazia!");
+            return '.';
+        }
+    } 
+    
+	public void exibe() {
+		No_pilha aux = ini;
+		
+		System.out.print("Pilha de ponteiros => TOPO [");
+		for(int i = 0; i < size(); i++) {
+			System.out.print(aux.info);
+			aux = aux.prox;
+		}		
+		System.out.println("] FUNDO");
+	}
+	
+	public boolean underflow() {
+		return size() == 0;
+	}
+	
+	public boolean overflow() {
+		return size() >= max;
+	}
+}
